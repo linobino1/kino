b5464a27231d3c709764a7ea9b6655e3dd569c2f
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-04-23 15:52:43 +0200

    type user in request context

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,11 +1,12 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
-
 import type { Response } from 'express';
 import type { Payload } from 'payload';
+import type { User } from '@cms/payload-types';
 
 export interface RemixRequestContext {
     payload: Payload;
+    user: User;
     res: Response;
 }
 
