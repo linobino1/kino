0350edc2ec734a70bf115470db13f8bbf943ba22
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-05-12 14:27:38 +0200

    add more flexibility for filters

--- app/routes/__main/archive/index.tsx
+++ app/routes/__main/archive/index.tsx
@@ -1,27 +1,20 @@
-import type { ActionFunction, LoaderArgs } from "@remix-run/node";
-import { useActionData, useLoaderData } from "@remix-run/react";
+import type { ActionFunction, LoaderArgs} from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useActionData, useFetcher, useLoaderData } from "@remix-run/react";
 import { Page } from "~/components/Page";
 import { FilmPrintsList } from "~/components/FilmPrintsList";
 import classes from "./index.module.css";
 import i18next from "~/i18next.server";
 import { useTranslation } from "react-i18next";
-import { Form } from "~/components/Form";
 import Button from "~/components/Button";
 import type { Payload } from "payload";
+import type { FilmPrint } from "payload/generated-types";
+import type { AppliedFilter} from "~/util/filter";
+import { Filters } from "~/util/filter";
+import { useRef } from "react";
 import type { Where } from "payload/types";
-import type { FilmPrint, Movie, Person } from "payload/generated-types";
 
-type Filter = {
-  value: any
-  count: number
-  label?: string
-}
-type Filters = {
-  director?: Filter[]
-  decade?: Filter[]
-}
-
-export const loader = async ({ request, params, context: { payload }}: LoaderArgs) => {
+export const loader = async ({ request, context: { payload }}: LoaderArgs) => {
   const locale = await i18next.getLocale(request);
   const page = await payload.findGlobal({
     slug: 'archive',
@@ -29,212 +22,137 @@ export const loader = async ({ request, params, context: { payload }}: LoaderArg
   });
   
   // get all published film prints
+  const filters = getFilters(payload);
   const filmPrints = await payload.find({
     collection: 'filmPrints',
     locale,
     depth: 7,
-    where: await getWhere(request),
+    where: filters.getWhereClause(),
   });
-  
-  // compute decades
   
   return {
     page,
+    filters: await filters.getApplied(),
     items: filmPrints.docs || [],
-    filters: await getFilters(payload, request),
   }
 };
 
 export const action: ActionFunction = async ({ request, context: { payload }}) => {
   const locale = await i18next.getLocale(request);
+  const formData = await request.formData();
+  const filters = getFilters(payload, formData);
   const filmPrints = await payload.find({
     collection: 'filmPrints',
     locale,
     depth: 7,
-    where: await getWhere(request),
+    where: filters.getWhereClause(),
   });
   
-  return {
+  const data = {
     items: filmPrints.docs || [],
-    filters: await getFilters(payload, request),
+    filters: await filters.getApplied(),
+    query: formData.get('query') || '',
+    depth: 7,
   }
+  return json(data, { status: 200 });
 }
 
-/**
- * build where clause for filtering from form data
- */
-const getWhere = async (request: globalThis.Request, extraWhere?: Where) => {
-  let data: FormData | null = null;
-  try {
-    data = await request.formData();
-  } catch {}
-
-  const res: Where = {
-    and: [
+const getFilters = (payload: Payload, formData?: FormData): Filters => {
+  const query = formData && formData.get('query');
+  const globalCause: Where | undefined = query ? {
+    or: [
       {
-        _status: {
-          equals: 'published',
+        'movie.originalTitle': {
+          contains: query,
+        },
+      },
+      {
+        'movie.title': {
+          contains: query,
+        },
+      },
+      {
+        'movie.directors.name': {
+          contains: query,
+        },
+      },
+      {
+        'movie.cast.name': {
+          contains: query,
+        },
+      },
+      {
+        'movie.synopsis': {
+          contains: query,
         },
       },
     ],
-  }
-
-  if (extraWhere) res.and?.push(extraWhere);
-
-  if (data && data.get('filter_isHfgProduction')) {
-    res.and?.push({
-      'movie.isHfgProduction': {
-        equals: true,
+  } : undefined;
+  const filters = new Filters({
+    collection: 'filmPrints',
+    payload,
+    filters: [
+      {
+        name: 'movie.isHfgProduction',
       },
-    });
-  }
-
-  if (data && data.get('filter_exclude_isHfgProduction')) {
-    res.and?.push({
-      'movie.isHfgProduction': {
-        equals: false,
+      {
+        name: 'movie.directors.name',
+        labelOff: 'All Directors',
       },
-    });
-  }
-  
-  return res;
-}
-
-/**
- * count how many items would remain after filtering with another where clause
- */
-export const countWithFilter = async (payload: Payload, request: globalThis.Request, extraWhere: Where) => {
-  console.log('countWithFilter', JSON.stringify(await getWhere(request, extraWhere)));
-  const res = await payload.find({
-    collection: 'filmPrints',
-    where: await getWhere(request, extraWhere),
-    depth: 0,
+    ],
+    globalCause,
   });
-  return res.totalDocs
-}
-
-/**
- * helper function to get all decades with their total number of items
- */
-const getFilters = async (payload: Payload, request: globalThis.Request): Promise<Filters> => {
-  // directors
-  const filmprints = (await payload.find({
-    collection: 'filmPrints',
-    depth: 2,
-  })).docs;
-  const allDirectors: Person[] = [];
-  // collect all directors
-  filmprints.forEach((filmprint) => {
-    allDirectors.push(...((filmprint.movie as Movie).directors as Person[]));
-  });
-  // remove duplicates
-  const directors = allDirectors.filter((director, index) => {
-    return allDirectors.findIndex((d) => d.id === director.id) === index;
-  });
-  
-  const director: Filter[] = []; 
-  for (const doc of directors) {
-    console.log('doc', doc);
-    director.push({
-      count: await countWithFilter(payload, request, {
-        'movie.directors': {
-          equals: doc.id,
-        },
-      }),
-      value: doc.id,
-      label: doc.name,
-    });
-  }
-
-  // decades
-  // get film prints with the oldest and youngest movie
-  const oldest = await payload.find({
-    collection: 'filmPrints',
-    where: await getWhere(request),
-    sort: 'movie.year',
-    limit: 1,
-  });
-  const youngest = await payload.find({
-    collection: 'filmPrints',
-    where: await getWhere(request),
-    sort: '-movie.year',
-    limit: 1,
-  });
-  const decade: Filter[] = [];
-  const firstYear = (oldest.docs[0].movie as Movie)?.year;
-  const lastYear = (youngest.docs[0].movie as Movie)?.year;
-  for (let year = firstYear - (firstYear % 10); year <= lastYear - (lastYear % 10); year += 10) {
-    decade.push({
-      count: await countWithFilter(payload, request, {
-        'movie.year': {
-          greater_than_equal: year,
-          less_than: year + 10,
-        },
-      }),
-      value: year,
-    });
-  }
-  return {
-    decade,
-    director,
-  }
+  filters.apply(formData);
+  return filters;
 }
 
 export default function Index() {
   const loaderData = useLoaderData<typeof loader>();
   const actionData = useActionData<typeof action>();
   const { page } = loaderData;
-  const data: { items: FilmPrint[], filters: Filters} = actionData || loaderData;
-  const { items, filters } = data;
+  const data: { items: FilmPrint[], filters: AppliedFilter[], query?: string } = actionData || loaderData;
+  const { items, filters, query } = data;
   const { t } = useTranslation();
-
+  const form = useRef<HTMLFormElement>(null);
+  const filterFetcher = useFetcher();
+  
   return (
     <Page layout={page.layout}>
-      <Form method="POST" className={classes.search}>
+      <filterFetcher.Form
+        ref={form}
+        method="POST"
+        className={classes.search}
+      >
         <input
           type="search"
-          name="search"
+          name="query"
+          defaultValue={query}
           placeholder={t('search...') || ''}
         />
-        <input
-          type="checkbox"
-          name="filter_isHfgProduction"
-          aria-label={t('show HfG productions only') || ''}
-        />
-        <input
-          type="checkbox"
-          name="filter_exclude_isHfgProduction"
-          aria-label={`${t('exclude HfG productions')} (${'jj'})`}
-        />
-        <select
-          name="filter_genre"
-          aria-label={t('genre') || ''}
-        >
-          { filters.decade?.map(decade => (
-            <option
-              key={decade.value}
-              value={decade.value}
-            >{`${decade.value}s (${decade.count})`}</option>
-          ))}
-        </select>
-        <select
-          name="filter_decade"
-          aria-label={t('director') || ''}
-        >
-          { filters.director?.map(item => (
-            <option
-              key={item.value}
-              value={item.value}
-            >{`${item.label} (${item.count})`}</option>
-          ))}
-        </select>
-          
         <Button
-          type="submit"
+          // type="submit"
+          onClick={() => form.current?.submit()}
         >
           {t('submit')}
         </Button>
-      </Form>
+        { filters.map((filter) => (
+          <select
+            key={filter.name}
+            name={filter.name}
+            // onChange={(e) => filterFetcher.submit(e.target.form, { replace: true })}
+            onChange={() => form.current?.submit()}
+            // onChange={() => filterFetcher.submit(form.current, { replace: true })}
+            value={filter.value}
+          >
+            { filter.options.map(option => (
+              <option
+                key={option.value}
+                value={option.value}
+              >{`${option.label} (${option.count})`}</option>
+            ))}
+          </select>
+        ))}
+      </filterFetcher.Form>
       <FilmPrintsList items={items} className={classes.screeningsList} />
     </Page>
   );
---
+++ app/util/filter/index.ts
@@ -0,0 +1,206 @@
+import type { Payload } from "payload";
+import type { Where } from "payload/types";
+
+export type FilterProps = {
+  name: string;
+  collection: string;
+  payload: Payload;
+  labelOff?: string;
+  parent: Filters;
+}
+
+export type FilterOption = {
+  value: string;
+  label?: string;
+  count: number;
+}
+
+export type FiltersProps = {
+  payload: Payload;
+  collection: string;
+  filters: Omit<FilterProps, 'payload' | 'collection' | 'parent'>[];
+  globalCause?: Where;
+}
+
+export type AppliedFilter = {
+  name: string;
+  value: any;
+  options: FilterOption[];
+}
+
+export class Filter {
+  props = {} as FilterProps;
+  activeValue = null as any;
+  
+  constructor(props: FilterProps) {
+    this.props = props;
+  }
+  
+  getWhereClause(value: any): Where {
+    if (value === null) return {
+      [this.props.name]: {},
+    }
+    if (value.length) return {
+      [this.props.name]: {
+        contains: value,
+      },
+    }
+    return {
+      [this.props.name]: {
+        equals: value,
+      },
+    }
+  }
+ 
+  /**
+   * allows to access nested fields using dot notation
+   * @param item the doc to get the field from
+   * @param name a field name or a dot separated path to a field
+   * @returns the value of the field
+   */
+  getField(item: any, name: string): any {
+    const fields = this.props.name.split('.');
+    if (fields.length > 1) {
+      return fields.reduce((acc, field) => {
+        if (Array.isArray(acc)) {
+          // treat hasMany and array fields, return an array of values of the field for each item
+          return acc.map((x) => x[field]);
+        } else {
+          return acc[field];
+        }
+      }, item);
+    } else {
+      return item[name];
+    }
+  }
+  
+  async getPossibleValues(): Promise<any[]> {
+    // get all items in the collection
+    const items = (await this.props.payload.find({
+      // @ts-ignore this.collection must be a valid collection slug
+      collection: this.props.collection,
+    })).docs;
+    
+    // remove duplicate values
+    const values = items.map((item) => this.getField(item, this.props.name)).flat();
+    const uniqueValues  = values.filter((value, index) => {
+      const _value = JSON.stringify(value);
+      return index === values.findIndex(obj => {
+        return JSON.stringify(obj) === _value;
+      });
+    });
+    return uniqueValues;
+  }
+  
+  async getOptions(): Promise<FilterOption[]> {
+    const values = await this.getPossibleValues();
+    return [
+      {
+        value: '',
+        label: this.props.labelOff || 'off',
+        count: await this.getCount(null),
+      },
+      ...await Promise.all(values.map(async (value) => {
+        return {
+          value,
+          label: `${value}`,
+          count: await this.getCount(value),
+        }
+      })),
+    ];
+  }
+  
+  async getCount(value: any): Promise<number> {
+    const where = this.props.parent.getWhereClause();
+    
+    // TODO somewhere here seems to be a bug, the count is not correct for some
+    // docs if a global cause is applied and true
+    // replace this fields value in the where clause with the probe value
+    if (where.and) {
+      const clause = where.and.find((item) => item[this.props.name] !== undefined);
+      if (clause) {
+        if (value === null) {
+          // remove the clause
+          where.and = where.and.filter((item) => item !== clause);
+        }
+        clause[this.props.name] = this.getWhereClause(value)[this.props.name];
+      } else if (value !== null) {
+        where.and.push(this.getWhereClause(value));
+      }
+    }
+    
+    const res = (await this.props.payload.find({
+      // @ts-ignore this.collection must be a valid collection slug
+      collection: this.props.collection,
+      where,
+      depth: 7,
+    })).totalDocs;
+    return res;
+    // IDEA: use aggregate to get the count, might be faster
+    // const model = this.props.payload.collections[this.props.collection].Model;
+    // const res = await model.aggregate([
+    //   { $match: where },
+    //   { $group: { _id: null, count: { $sum: 1 } } },
+    // ]);
+    // return res[0]?.count || 0;
+  }
+  
+  async getApplied(): Promise<AppliedFilter> {
+    return {
+      name: this.props.name,
+      value: this.activeValue,
+      options: (await this.getOptions()).map((option) => ({
+        count: option.count,
+        value: option.value,
+        label: option.label || `${option.value}`,
+      })),
+    }
+  }
+}
+
+export class Filters {
+  props: FiltersProps;
+  filters: Filter[];
+  
+  constructor(props: FiltersProps) {
+    this.props = props;
+
+    this.filters = this.props.filters.map((filter) => new Filter({
+      ...filter,
+      payload: this.props.payload,
+      collection: this.props.collection,
+      parent: this,
+    }));
+  }
+  
+  getWhereClauses(): Where[] {
+    const res = this.filters
+      .filter((filter) => filter.activeValue !== null)
+      .map((filter) => filter.getWhereClause(filter.activeValue));
+    if (this.props.globalCause) {
+      res.push(this.props.globalCause);
+    }
+    return res;  
+  }
+
+  getWhereClause(): Where {
+    const res = {
+      and: this.getWhereClauses(),
+    }
+    return res;
+  }
+  
+  /**
+   * apply the filters from a form data
+   * @param formData request body
+   */
+  apply(formData?: FormData): void {
+    this.filters.forEach((filter) => {
+      filter.activeValue = formData?.get(filter.props.name) || null;
+    });
+  }
+  
+  async getApplied(): Promise<AppliedFilter[]> {
+    return await Promise.all(this.filters.map(async (filter) => filter.getApplied()));
+  }
+}
