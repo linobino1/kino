9a1fbef61f82182ec1961e7b2a64141c4b948e4c
Leo <leo.hilsheimer@gmail.com>
2023-05-23 09:52:16 +0200

    fallback language en so translation keys are english text

--- app/i18n.ts
+++ app/i18n.ts
@@ -8,9 +8,10 @@ export interface Options extends InitOptions {
 export const options: Options = {
   // This is the list of languages your application supports
   supportedLngs: ["en", "de"],
-  // This is the language you want to use in case
-  // the user language is not in the supportedLngs
-  fallbackLng: "de",
+  // This is the language you want to use in case the user language is not in the supportedLngs
+  // Also, use the language that you are using in the source code here, so the translation keys will be used as texts
+  // the translations for it
+  fallbackLng: "en",
   // The default namespace of i18next is "translation", but you can customize it here
   defaultNS: "common",
   fallbackNS: "common",
