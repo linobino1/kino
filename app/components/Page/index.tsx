89f1fc5a77c9a0935ecf221f28d7dfa2f6674d0c
Leo <leo.hilsheimer@gmail.com>
2023-05-30 13:03:16 +0200

    fix Page component, layout parameter optional

--- app/components/Page/index.tsx
+++ app/components/Page/index.tsx
@@ -4,19 +4,21 @@ import classes from "./index.module.css";
 import type { PageLayout } from "cms/fields/pageLayout";
 
 export type Type = {
-  layout: PageLayout
+  layout?: PageLayout
   children?: React.ReactNode
 }
 
 export const Page: React.FC<Type> = ({
   layout, children,
 }) => (
-  <div className={classes.page} data-layout-type={layout.type}>
-    <Blocks
-      blocks={layout.blocks}
-    >
-      {children}
-    </Blocks>
+  <div className={classes.page} data-layout-type={layout?.type}>
+    { layout?.blocks ? (
+      <Blocks
+        blocks={layout.blocks}
+      >
+        {children}
+      </Blocks>
+    ) : children}
   </div>
 );
 
