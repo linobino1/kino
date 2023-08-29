4cba65f06f7caf8b92fe20dc26d0eacaaf86338d
Leo <leo.hilsheimer@gmail.com>
2023-08-29 13:56:57 +0100

    add migration for adding languageversions.abbreviation

---
+++ migrations/20230829125038-add-languageversions.abbreviation.js
@@ -0,0 +1,19 @@
+module.exports = {
+  async up(db, client) {
+    // add languageversions.abbreviation
+    await db.collection('languageversions').updateMany({}, {
+      $set: {
+        abbreviation: ''
+      },
+    });
+  },
+
+  async down(db, client) {
+    // remove languageversions.abbreviation
+    await db.collection('languageversions').updateMany({}, {
+      $unset: {
+        abbreviation: '',
+      },
+    });
+  }
+};
