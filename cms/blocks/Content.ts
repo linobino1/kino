40d00d4774eda8f9dbc786853561c496ee559720
Leo <leo.hilsheimer@gmail.com>
2023-09-02 16:56:01 +0100

    translate content block label

--- cms/blocks/Content.ts
+++ cms/blocks/Content.ts
@@ -1,7 +1,12 @@
 import type { Block } from "payload/types";
+import { t } from '../i18n';
 
 export const Content: Block = {
   slug: 'content',
+  labels: {
+    singular: t('Content'),
+    plural: t('Content'),
+  },
   fields: [
     {
       name: 'content',
