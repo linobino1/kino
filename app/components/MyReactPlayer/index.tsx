e1323b3a86aa45b3252d6d52aefa80c11b8965b0
Leo <leo.hilsheimer@gmail.com>
2023-09-09 22:37:40 +0200

    improve video error handling

--- app/components/MyReactPlayer/index.tsx
+++ app/components/MyReactPlayer/index.tsx
@@ -1,19 +1,19 @@
+import { useState } from "react"
 import type { ReactPlayerProps } from "react-player"
+import { useTranslation } from "react-i18next"
 import ReactPlayer from "react-player"
 import classes from './index.module.css'
-import { useState } from "react"
 
 export interface Props extends ReactPlayerProps {
 }
 
 export const MyReactPlayer: React.FC<Props> = (props) => {
-  // is loading
+  const { t } = useTranslation();
+
+  // loading and error state
   const [isLoading, setIsLoading] = useState(true);
-  const handleReady = (player: any) => {
-    console.log('player', player)
-    setIsLoading(false);
-    player.wrapper.className = classes.player
-  }
+  const [isError, setIsError] = useState(false);
+
   return (
     <div className={`${classes.container} ${isLoading && classes.loading}`}>
       <ReactPlayer
@@ -21,8 +21,21 @@ export const MyReactPlayer: React.FC<Props> = (props) => {
         width={'100%'}
         height={'100%'}
         controls={true}
-        onReady={handleReady}
+        onReady={() => setIsLoading(false)}
+        onError={() => setIsError(true)}
       />
+      { isError && (
+        <p className={classes.message}>
+          { t('Video is not available.') }
+          <br />
+          { typeof props.url === 'string' ? props.url : ''}
+        </p>
+      )}
+      { isLoading && (
+        <p className={classes.message}>
+          { t('Loading...') }
+        </p>
+      )}
     </div>
   )
 }
