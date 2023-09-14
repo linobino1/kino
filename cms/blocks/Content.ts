752f19ba7175996fa256b150449bb1cad4954fef
Leo <leo.hilsheimer@gmail.com>
2023-09-14 10:16:56 +0200

    same richtext field config for content block as for posts

--- cms/blocks/Content.ts
+++ cms/blocks/Content.ts
@@ -1,5 +1,6 @@
 import type { Block } from "payload/types";
 import { t } from '../i18n';
+import video from '../fields/richtext/video';
 
 export const Content: Block = {
   slug: 'content',
@@ -12,6 +13,22 @@ export const Content: Block = {
       name: 'content',
       type: 'richText',
       localized: true,
+      admin: {
+        elements: [
+          'h2',
+          'h3',
+          'h4',
+          'h5',
+          'h6',
+          'link',
+          'ol',
+          'ul',
+          'indent',
+          'upload',
+          video,
+        ],
+        leaves: ['bold', 'italic', 'underline', 'strikethrough'],
+      },
     },
   ],
 }
