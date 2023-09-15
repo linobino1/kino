7306203c2d649b0ee36ac840c1a1940995a254c9
Leo <leo.hilsheimer@gmail.com>
2023-09-15 10:28:30 +0200

    fix migration

--- migrations/20230914212439-rename-seasons.js
+++ migrations/20230914212439-rename-seasons.js
@@ -1,11 +1,11 @@
 module.exports = {
   async up(db, client) {
     // rename screeningSeasons to seasons
-    await db.collection('screeningSeasons').rename('seasons');
+    await db.collection('screeningseasons').rename('seasons');
   },
 
   async down(db, client) {
     // rename seasons to screeningSeasons
-    await db.collection('seasons').rename('screeningSeasons');
+    await db.collection('seasons').rename('screeningseasons');
   }
 };
