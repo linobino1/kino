ccdd40f1bbd7b8b59118caddd3599c5a6f82d22f
Leo <leo.hilsheimer@gmail.com>
2023-09-23 11:18:53 +0200

    allow default props in <Heading> component

--- app/components/Heading/index.tsx
+++ app/components/Heading/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-export type Type = {
+export interface Type extends React.HTMLAttributes<HTMLHeadingElement> {
   text?: string
   children?: React.ReactNode
 }
 
-export const Heading: React.FC<Type> = ({ text, children }) => (
-  <h1>{text || children}</h1>
-);
+export const Heading: React.FC<Type> = (props) => {
+  const { text, children } = props;
+  return (
+    <h1 {...props}>{text || children}</h1>
+  );
+};
 
 export default Heading;
