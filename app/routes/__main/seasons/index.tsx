097740173b0708068b061ad3d2c2734c383bb7c5
Leo <leo.hilsheimer@gmail.com>
2023-09-23 10:30:50 +0200

    add pagination to seasons page

--- app/routes/__main/seasons/index.tsx
+++ app/routes/__main/seasons/index.tsx
@@ -1,4 +1,4 @@
-import type { LoaderArgs, MetaFunction } from "@remix-run/node";
+import { redirect, type LoaderArgs, type MetaFunction } from "@remix-run/node";
 import type { Media } from "payload/generated-types";
 import { useLoaderData } from "@remix-run/react";
 import i18next from "~/i18next.server";
@@ -7,7 +7,7 @@ import { pageDescription, pageKeywords, pageTitle } from "~/util/pageMeta";
 import { ErrorPage } from "~/components/ErrorPage";
 import classes from './index.module.css';
 import Image from "~/components/Image";
-
+import Pagination from "~/components/Pagination";
 
 export const ErrorBoundary = ErrorPage;
 
@@ -17,12 +17,22 @@ export const loader = async ({ request, context: { payload }}: LoaderArgs) => {
     slug: 'seasonsPage',
     locale,
   });
+  const pageNumber  = parseInt(new URL(request.url).searchParams.get('page') || '1');
   const seasons = (await payload.find({
     collection: 'seasons',
     locale,
     depth: 3,
-  })).docs;
+    limit: 10,
+    pagination: true,
+    page: pageNumber,
+  }));
   
+  // Redirect to the last page if the requested page is greater than the total number of page
+  if (pageNumber > seasons.totalPages) {
+    throw redirect(`?page=${seasons.totalPages}`, {
+      status: 302,
+    });
+  }
   return {
     page,
     seasons,
@@ -40,21 +50,26 @@ export default function Seasons() {
 
   return (
     <Page layout={page.layout}>
-      <ul className={classes.seasons}>
-        { seasons.map((season) => (
-          <li key={season.id}>
-            <a href={`/seasons/${season.slug}`}>
-              <Image
-                image={season.header as Media}
-                alt={season.name}
-              />
-              <div className={classes.overlay}>
-                <h2>{season.name}</h2>
-              </div>
-            </a>
-          </li>
-        ))}
-      </ul>
+      <main>
+        { seasons.docs?.length ? (
+          <ul className={classes.seasons}>
+            { seasons.docs.map((season) => (
+              <li key={season.id}>
+                <a href={`/seasons/${season.slug}`}>
+                  <Image
+                    image={season.header as Media}
+                    alt={season.name}
+                  />
+                  <div className={classes.overlay}>
+                    <h2>{season.name}</h2>
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        ) : null }
+        <Pagination {...seasons} linkProps={{ prefetch: 'intent' }}/>
+      </main>
     </Page>
   )
 }
