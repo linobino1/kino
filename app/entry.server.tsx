49245db299ff684def2eb860f8fc6cbe2792ab73
Leo <leo.hilsheimer@gmail.com>
2024-03-04 14:31:33 +0100

    fix type error in entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,5 +1,8 @@
 import { PassThrough } from "stream";
-import type { EntryContext } from "@remix-run/node";
+import {
+  createReadableStreamFromReadable,
+  type EntryContext,
+} from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
@@ -48,11 +51,12 @@ export default async function handleRequest(
       {
         [callbackName]: () => {
           let body = new PassThrough();
+          const stream = createReadableStreamFromReadable(body);
 
           responseHeaders.set("Content-Type", "text/html");
 
           resolve(
-            new Response(body, {
+            new Response(stream, {
               headers: responseHeaders,
               status: didError ? 500 : responseStatusCode,
             })
