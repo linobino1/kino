e816d743a72e25ce94e756fcf92712a4969c7f7a
Leo <leo.hilsheimer@gmail.com>
2024-04-05 09:11:47 +0200

    new newsletter form with name fields using `use-mailchimp-form`

--- app/components/newsletter-signup/index.tsx
+++ app/components/newsletter-signup/index.tsx
@@ -1,40 +1,83 @@
-import React from "react";
-import Mailchimp from "react-mailchimp-form";
+import { Form } from "@remix-run/react";
+import { useFormFields, useMailChimpForm } from "use-mailchimp-form";
 import classes from "./index.module.css";
-import environment from "~/util/environment";
 import { useTranslation } from "react-i18next";
+import environment from "~/util/environment";
+import { type HTMLAttributes } from "react";
 
-export type Props = {
-  className?: string;
-};
+export interface Props extends HTMLAttributes<HTMLDivElement> {}
 
-export const NewsletterSignup: React.FC<Props> = ({ className }) => {
-  const url = environment().MAILCHIMP_SIGNUP_URL;
+const NewsletterSignup: React.FC<Props> = ({ className, ...props }) => {
   const { t } = useTranslation();
+  const url = environment().MAILCHIMP_SIGNUP_URL;
+  const { loading, error, success, message, handleSubmit, reset } =
+    useMailChimpForm(url);
+  const { fields, handleFieldChange } = useFormFields({
+    EMAIL: "",
+    FNAME: "",
+    LNAME: "",
+  });
 
   return (
-    <div className={`${classes.container} ${className}`}>
-      <div className={classes.title}>{t("newsletter.title")}</div>
-      <Mailchimp
-        className={classes.form}
-        action={url}
-        fields={[
-          {
-            name: "EMAIL",
-            placeholder: t("newsletter.placeholder"),
-            type: "email",
-            required: true,
-          },
-        ]}
-        messages={{
-          sending: t("newsletter.sending"),
-          success: t("newsletter.success"),
-          error: t("newsletter.error"),
-          empty: t("newsletter.empty"),
-          duplicate: t("newsletter.duplicate"),
-          button: "OK",
+    <div
+      {...props}
+      className={[classes.container, className].filter(Boolean).join(" ")}
+    >
+      <div className={classes.title}>{t("newsletter.cta")}</div>
+      {success && (
+        <div className={classes.success}>
+          <p>{t("newsletter.success")}</p>
+          <button type="button" onClick={reset}>
+            {t("newsletter.subscribeAgain")}
+          </button>
+        </div>
+      )}
+      <Form
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSubmit(fields);
         }}
-      />
+        className={[classes.form, success && classes.hidden].join(" ")}
+      >
+        <label htmlFor="FNAME">{t("newsletter.firstName")}</label>
+        <input
+          type="text"
+          name="FNAME"
+          id="FNAME"
+          autoComplete="given-name"
+          value={fields.FNAME}
+          onChange={handleFieldChange}
+          placeholder={t("newsletter.firstName")}
+          required
+        />
+        <label htmlFor="LNAME">{t("newsletter.lastName")}</label>
+        <input
+          type="text"
+          name="LNAME"
+          id="LNAME"
+          autoComplete="family-name"
+          value={fields.LNAME}
+          onChange={handleFieldChange}
+          placeholder={t("newsletter.lastName")}
+          required
+        />
+        <label htmlFor="EMAIL">{t("newsletter.email")}</label>
+        <input
+          type="email"
+          name="EMAIL"
+          id="EMAIL"
+          autoComplete="email"
+          value={fields.EMAIL}
+          onChange={handleFieldChange}
+          placeholder={t("newsletter.email")}
+          required
+        />
+        {error && <p className={classes.error}>{error}</p>}
+        {message && <p>{message}</p>}
+        <button type="submit">
+          {loading ? t("newsletter.sending") : t("newsletter.subscribe")}
+        </button>
+      </Form>
     </div>
   );
 };
