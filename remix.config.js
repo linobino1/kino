ffd261d1898ff0671b6a97590a9d340eea141678
Leo <leo.hilsheimer@gmail.com>
2024-08-26 16:36:46 +0200

    bump remix v2.11.2 with all future flags enabled

--- remix.config.js
+++ remix.config.js
@@ -3,4 +3,11 @@ module.exports = {
   serverDependenciesToBundle: ["html-escaper"],
   ignoredRouteFiles: ["**/*.css"],
   serverModuleFormat: "cjs",
+  future: {
+    v3_fetcherPersist: true,
+    v3_relativeSplatPath: true,
+    v3_throwAbortReason: true,
+    unstable_singleFetch: true,
+    unstable_lazyRouteDiscovery: true,
+  },
 };
