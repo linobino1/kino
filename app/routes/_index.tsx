d048ddd823e0c4479bea22260aa13370ab5409d9
Leo <leo.hilsheimer@gmail.com>
2024-08-27 08:54:24 +0200

    redirect landing page to localized news

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,12 @@
-import { redirect } from "@remix-run/node";
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
+import i18next from "~/i18next.server";
 
 /**
  * Redirect to /news
  */
-export const loader = () => {
-  throw redirect("/news");
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const lang = await i18next.getLocale(request);
+  throw redirect(`/${lang}/news`);
 };
 
 export default function Index() {
