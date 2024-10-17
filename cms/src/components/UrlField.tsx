ef45fd93195c8e3852b052ef6fdb895263e42a10
Leo <leo.hilsheimer@gmail.com>
2024-10-17 20:32:52 +0200

    remove duplicate label of URLField

--- cms/src/components/UrlField.tsx
+++ cms/src/components/UrlField.tsx
@@ -7,16 +7,11 @@ import { env } from '@/util/env'
 const UrlField: TextFieldClientComponent = () => {
   const { value: relativeUrl } = useField({})
   const absoluteUrl = `${env.FRONTEND_URL}${relativeUrl}`
-  return (
-    <div className="field-type text">
-      <label className="field-label">URL</label>
-      {typeof relativeUrl === 'string' && (
-        <a href={absoluteUrl} target="_blank" rel="noreferrer">
-          {relativeUrl}
-        </a>
-      )}
-    </div>
-  )
+  return typeof relativeUrl === 'string' ? (
+    <a href={absoluteUrl} target="_blank" rel="noreferrer">
+      {relativeUrl}
+    </a>
+  ) : null
 }
 
 export default UrlField
