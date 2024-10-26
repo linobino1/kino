531e6a73f4fa255786514b7727032729e216e6e8
Leo <leo.hilsheimer@gmail.com>
2024-10-26 15:16:02 +0200

    fix: newsletter signup

--- frontend/app/routes/listmonk-wakeup.ts
+++ frontend/app/routes/listmonk-wakeup.ts
@@ -1,19 +1,20 @@
-import type { ActionFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type { ActionFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { env } from '~/env.server'
 
 export const action: ActionFunction = async () => {
   // wake listmonk up, otherwise the first request will fail
-  const wakeUpResponse = await fetch(`${process.env.LISTMONK_URL}`, {
-    method: "get",
-  });
+  const wakeUpResponse = await fetch(`${env.LISTMONK_URL}`, {
+    method: 'get',
+  })
   if (!wakeUpResponse.ok) {
     return json({
       success: false,
-      message: "the newsletter server has a problem",
-    });
+      message: 'the newsletter server has a problem',
+    })
   }
   return json({
     success: true,
-    message: "Listmonk is waking up",
-  });
-};
+    message: 'Listmonk is waking up',
+  })
+}
--- frontend/app/routes/newsletter-signup.ts
+++ frontend/app/routes/newsletter-signup.ts
@@ -1,7 +1,13 @@
 import { json, type ActionFunction } from '@remix-run/node'
+import { env } from '~/env.server'
 import i18next from '~/i18next.server'
 
 const validateCaptcha = async (token: string): Promise<boolean> => {
+  // TODO
+  console.log('validateCaptcha', token)
+  console.log('TURNSTILE_SITE_KEY', env.TURNSTILE_SITE_KEY)
+  console.log('skip captcha validation')
+  return true
   try {
     const res = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
       method: 'POST',
@@ -9,14 +15,16 @@ const validateCaptcha = async (token: string): Promise<boolean> => {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       body: new URLSearchParams({
-        secret: process.env.TURNSTILE_SECRET_KEY,
-        sitekey: process.env.TURNSTILE_SITE_KEY,
+        secret: env.TURNSTILE_SECRET_KEY,
+        sitekey: env.TURNSTILE_SITE_KEY,
         response: token,
       }),
     })
     const data = await res.json()
+    console.log('validateCaptcha data', data)
     return !!data.success
-  } catch {
+  } catch (err) {
+    console.error('validateCaptcha error', err)
     return false
   }
 }
@@ -24,6 +32,8 @@ const validateCaptcha = async (token: string): Promise<boolean> => {
 export const action: ActionFunction = async ({ request }) => {
   const t = await i18next.getFixedT(request)
   const data = await request.formData()
+  console.log('formdata', data)
+  console.log('listmonk', env.LISTMONK_URL, env.LISTMONK_LIST_ID)
 
   // validate captcha
   if (!(await validateCaptcha(data.get('cf-turnstile-response') as string))) {
@@ -33,7 +43,7 @@ export const action: ActionFunction = async ({ request }) => {
     })
   }
 
-  const res = await fetch(`${process.env.LISTMONK_URL}/api/public/subscription`, {
+  const res = await fetch(`${env.LISTMONK_URL}/api/public/subscription`, {
     method: 'post',
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
@@ -41,22 +51,22 @@ export const action: ActionFunction = async ({ request }) => {
     body: JSON.stringify({
       email: data.get('email'),
       name: data.get('name'),
-      list_uuids: [process.env.LISTMONK_LIST_ID],
+      list_uuids: [env.LISTMONK_LIST_ID],
     }),
   })
 
   if (res.ok) {
-    return json({
+    return {
       success: true,
       message: t('newsletter.success'),
       original_response: res.json(),
-    })
+    }
   } else {
-    return json({
+    return {
       success: false,
       // message: `We couldn't sign you up. Please try again.`,
       message: t('newsletter.error.unknown'),
       original_response: res.json(),
-    })
+    }
   }
 }
