f6399df177910a03a0347acdb6b0c8752e6a7948
Leo <leo.hilsheimer@gmail.com>
2024-10-29 20:24:34 +0100

    fix: add hfg-archive detail page

---
+++ frontend/app/routes/_main.hfg-archive.$slug.tsx
@@ -0,0 +1,5 @@
+import FilmPrintDetailPage, { loader, headers, meta, ErrorBoundary } from './_main.filmprints.$slug'
+
+export { loader, headers, meta, ErrorBoundary }
+
+export default FilmPrintDetailPage
