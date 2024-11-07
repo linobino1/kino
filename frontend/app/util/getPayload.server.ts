39a69df5703eb74ff986fa43ee90801c0854cef5
Leo <leo.hilsheimer@gmail.com>
2024-11-07 21:56:31 +0100

    debug: database reads exploding

--- frontend/app/util/getPayload.server.ts
+++ frontend/app/util/getPayload.server.ts
@@ -4,4 +4,7 @@ import config from '@payload-config'
 
 // returns a singleton of the payload object, which is used to fetch data from the database and is typed to the schema
 export const getPayload = async () =>
-  await singleton('payload', async () => await _getPayload({ config }))
+  await singleton('payload', async () => {
+    console.log('calling getPayload()')
+    return await _getPayload({ config })
+  })
