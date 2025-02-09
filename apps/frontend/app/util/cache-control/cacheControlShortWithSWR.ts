cbfb3604b93b8ad6fd29a99be05962dc7b82207e
Leo <leo.hilsheimer@gmail.com>
2025-02-09 23:06:35 +0100

    wip: disable CDN caching for now

--- apps/frontend/app/util/cache-control/cacheControlShortWithSWR.ts
+++ apps/frontend/app/util/cache-control/cacheControlShortWithSWR.ts
@@ -1,6 +1,4 @@
 /**
  * cache for 5 minutes, and allow stale data to be served while revalidating for 1h
  */
-export const cacheControlShortWithSWR = `public, max-age=${60 * 5}, s-maxage=${
-  60 * 5
-}, stale-while-revalidate=${60 * 60}`
+export const cacheControlShortWithSWR = `public, max-age=0, s-maxage=0, stale-while-revalidate=${60 * 60}`
