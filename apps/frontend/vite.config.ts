7c566fb03c6b3c37d4c6398d729eff6756062637
Leo <leo.hilsheimer@gmail.com>
2025-02-13 09:39:37 +0100

    wip: use vite optimizeDeps over aliasing

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,25 +2,19 @@ import { reactRouter } from '@react-router/dev/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import UnoCSS from 'unocss/vite'
-import path from 'path'
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths(), UnoCSS()],
   optimizeDeps: {
-    exclude: ['@app/payload'],
+    exclude: [
+      '@app/payload',
+      'remix-i18next',
+      'payload',
+      '@payloadcms/richtext-lexical',
+      '@payloadcms/db-mongodb',
+    ],
   },
-  resolve:
-    process.NODE_ENV === 'development'
-      ? {
-          alias: {
-            'file-type': path.resolve(__dirname, '../../node_modules/file-type/index.js'),
-          },
-        }
-      : {},
   ssr: {
-    noExternal: ['remix-i18next', 'payload', '@payloadcms/richtext-lexical'].concat(
-      // not sure why this is necessary, but deployments on vercel throw runtime error if @payloadcms/db-mongodb is noExternal
-      process.env.NODE_ENV === 'development' ? ['@payloadcms/db-mongodb'] : [],
-    ),
+    noExternal: ['remix-i18next', 'payload', '@payloadcms/richtext-lexical'],
   },
 })
