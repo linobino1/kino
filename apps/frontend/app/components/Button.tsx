4bac8814b6b6365faedd1abbb24b404006bf1283
Leo <leo.hilsheimer@gmail.com>
2025-03-19 08:28:49 +0100

    chore: made trailer button slightly bigger

--- apps/frontend/app/components/Button.tsx
+++ apps/frontend/app/components/Button.tsx
@@ -10,14 +10,17 @@ export const Button: React.FC<Props> = ({ look = 'default', size = 'md', classNa
     <button
       {...props}
       className={cn(
-        'flex w-fit items-center justify-center px-3 py-1 text-xs font-semibold tracking-tight shadow transition-colors',
+        'flex w-fit items-center justify-center font-semibold tracking-tight shadow transition-colors',
         {
           'bg-theme-400 hover:bg-theme-300 text-white': look === 'default',
           'bg-red-500 text-white hover:bg-red-400': look === 'red',
           'hover:bg-theme-800 bg-black text-white': look === 'black',
           'border-gray-300 bg-white text-black hover:bg-gray-100 hover:text-black':
             look === 'white',
-          'px-2': size === 'sm',
+        },
+        {
+          'px-2 py-1 text-xs': size === 'sm',
+          'px-3 py-1 text-sm': size === 'md',
           'px-6 py-2 text-lg font-medium tracking-normal': size === 'lg',
         },
         className,
--- apps/frontend/app/components/TrailerButton.tsx
+++ apps/frontend/app/components/TrailerButton.tsx
@@ -2,18 +2,19 @@ import type { Movie } from '@app/types/payload'
 import { Button, type Props as ButtonProps } from './Button'
 import { Link } from 'react-router'
 import { useTranslation } from 'react-i18next'
+import { cn } from '@app/util/cn'
 
 type Props = ButtonProps & {
   movie: Movie
 }
 
-export const TrailerButton: React.FC<Props> = ({ movie, ...props }) => {
+export const TrailerButton: React.FC<Props> = ({ movie, className, ...props }) => {
   const { t } = useTranslation()
   return movie.trailer ? (
-    <Link className="contents" to={movie.trailer} target="_blank">
-      <Button look="black" className="flex items-center uppercase" {...props}>
+    <Link className="group contents" to={movie.trailer} target="_blank">
+      <Button look="black" className={cn('flex items-center uppercase', className)} {...props}>
         {t('Trailer')}
-        <span className="i-material-symbols:play-arrow relative left-1 text-lg" />
+        <div className="i-material-symbols:play-arrow translate-x-[2px] text-lg transition-transform group-hover:scale-[1.1]" />
       </Button>
     </Link>
   ) : null
