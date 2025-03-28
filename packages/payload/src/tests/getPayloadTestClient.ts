19bb914c14d2f9b97fb036a588307a8fd5f34113
Leo <leo.hilsheimer@gmail.com>
2025-03-28 11:28:01 +0100

    chore: fix typescript warning

--- packages/payload/src/tests/getPayloadTestClient.ts
+++ packages/payload/src/tests/getPayloadTestClient.ts
@@ -8,6 +8,7 @@ export const getPayloadTestClient = async (): Promise<Payload> => {
   return await getPayload({
     config: {
       ...config,
+      // @ts-expect-error why?
       db: mongooseAdapter({
         // @ts-expect-error globalThis.__MONGO_URI__ is created by vitest-mongodb
         url: globalThis.__MONGO_URI__,
