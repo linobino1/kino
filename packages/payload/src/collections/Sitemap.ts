60d8e126f9bc9780c4bf3a2c324bda4b3882ca82
Leo <leo.hilsheimer@gmail.com>
2025-07-27 22:38:35 +0200

    chore: change table name sitemaps -> sitemap

--- packages/payload/src/collections/Sitemap.ts
+++ packages/payload/src/collections/Sitemap.ts
@@ -2,6 +2,7 @@ import type { CollectionConfig } from 'payload'
 
 export const Sitemap: CollectionConfig<'sitemap'> = {
   slug: 'sitemap',
+  dbName: 'sitemap',
   admin: {
     hidden: true,
   },
