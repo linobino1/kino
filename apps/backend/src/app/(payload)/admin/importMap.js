eef016abedffb575d0dfaf397a1fa4a57d239b8d
Leo <leo.hilsheimer@gmail.com>
2026-05-16 15:23:48 +0200

    chore: upgrade pnpm, nextjs, payload, react-router

--- apps/backend/src/app/(payload)/admin/importMap.js
+++ apps/backend/src/app/(payload)/admin/importMap.js
@@ -42,6 +42,7 @@ import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc056
 import { TMDBMigration as TMDBMigration_b4543c28c07beb0098fe53a85cee14d7 } from '../../../views/TMDBMigration/index'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
+/** @type import('payload').ImportMap */
 export const importMap = {
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalField": RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e,
