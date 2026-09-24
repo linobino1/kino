36b59b86503549f20c4bcf0d927901cd48dc2d29
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2026-09-24 07:39:05 +0200

    fix: add allowedActionOrigins to react-router config to fix CSRF errors

--- apps/frontend/react-router.config.ts
+++ apps/frontend/react-router.config.ts
@@ -2,4 +2,11 @@ import type { Config } from '@react-router/dev/config'
 
 export default {
   ssr: true,
+  allowedActionOrigins: [
+    'kinoimblauensalon.de',
+    '*.kinoimblauensalon.de',
+    '127.0.0.1',
+    'localhost',
+    '*.local',
+  ],
 } satisfies Config
